23ba224e228975521c823cbdb73cb91a228e0408
Thomas Quatresooz <quatresooz.thomas@gmail.com>
2020-05-14 11:55:11 +0200

    finished the pomodoro with sfx

---
+++ src/components/pomotimer.js
@@ -0,0 +1,166 @@
+/**
+ *
+ *
+ */
+
+import React, {useState, useEffect} from "react";
+import {Card, Button, ButtonGroup, Modal} from "react-bootstrap";
+import click from "../audio/click.mp3";
+import ding from "../audio/ding.mp3";
+import tick from "../audio/tick.mp3";
+
+const PomoTimer = props => {
+    const [isRunning, setRunningState] = useState(false);
+    const [isBreak, setBreak] = useState(false);
+    const [timer, setTimer] = useState(props.workingSession);
+    //Modal
+    const [isShowing, setShowState] = useState(false);
+
+    function resetTimer() {
+        setRunningState(false);
+        setBreak(false);
+        setTimer(props.workingSession);
+    }
+
+    function handleClose() {
+        setShowState(false);
+    }
+
+    function beginBreak() {
+        handleClose();
+        setBreak(!isBreak);
+        setRunningState(true);
+    }
+
+    function playClickSound() {
+        const sound = new Audio(click);
+        sound.play();
+    }
+
+    function playDingSound() {
+        const sound = new Audio(ding);
+        sound.play();
+    }
+    function playTickSound() {
+        const sound = new Audio(tick);
+        sound.play();
+    }
+    function displaytimer() {
+        const seconds = timer % 60;
+        const minutes = parseInt(timer / 60) % 60;
+        function addLeadingZeroes(time) {
+            return time < 10 ? `0${time}` : time;
+        }
+        return `${addLeadingZeroes(minutes)}:${addLeadingZeroes(seconds)}`;
+    }
+
+    useEffect(() => {
+        setTimer(isBreak ? props.breakSession : props.workingSession);
+    }, [isBreak]);
+
+    useEffect(() => {
+        let interval;
+        if (isRunning) {
+            interval = setInterval(() => {
+                setTimer(prevTimer => prevTimer - 1);
+            }, 1000);
+        }
+        return () => clearInterval(interval);
+    }, [isRunning]);
+
+    useEffect(() => {
+        if (timer === 0) {
+            playDingSound();
+            document.title = isBreak ? "Back to work ?" : "Take a break !";
+            setRunningState(!isRunning);
+            setShowState(true);
+        } else {
+            document.title = displaytimer();
+        }
+        if (isRunning && timer <= 5 && timer > 0) {
+            playTickSound();
+        }
+    }, [timer]);
+
+    return (
+        <div>
+            <Card className={"my-auto"}>
+                <Card.Header className={"text-center"}>
+                    {isBreak ? "Pause Session" : "Working Session"}
+                </Card.Header>
+                <Card.Body>
+                    <div className={"d-flex flex-row align-items-center"}>
+                        <p
+                            className={"text-center w-75 "}
+                            style={{fontSize: "6rem"}}>
+                            {displaytimer()}
+                        </p>
+                        <ButtonGroup vertical className={"w-25"}>
+                            <Button
+                                disabled={isRunning}
+                                onClick={() => {
+                                    playClickSound();
+                                    setTimer(timer + 60);
+                                }}>
+                                {"+"}
+                            </Button>
+                            <Button
+                                disabled={timer <= 0}
+                                onClick={() => {
+                                    playClickSound();
+                                    setRunningState(!isRunning);
+                                }}>
+                                {isRunning ? "Stop" : "Play"}
+                            </Button>
+                            <Button
+                                onClick={() => {
+                                    playClickSound();
+                                    resetTimer();
+                                }}>
+                                {"Reset"}
+                            </Button>
+                            <Button
+                                disabled={
+                                    isRunning || parseInt(timer / 60) % 60 === 0
+                                }
+                                onClick={() => {
+                                    playClickSound();
+                                    setTimer(timer - 60);
+                                }}>
+                                {"-"}
+                            </Button>
+                        </ButtonGroup>
+                    </div>
+                </Card.Body>
+            </Card>
+            <Modal show={isShowing}>
+                <Modal.Header>
+                    <Modal.Title>{"Time out !"}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {`You just finished a ${
+                        isBreak ? "break" : "working"
+                    } session ! Ready for the next session ?`}
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant={"secondary"} onClick={handleClose}>
+                        {"Nope"}
+                    </Button>
+                    <Button variant={"primary"} onClick={beginBreak}>
+                        {`${isBreak ? "Time to code !" : "Take a break !"}`}
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
+};
+
+PomoTimer.defaultProps = {
+    //in seconds = 25 mins - 1500
+    workingSession: 10,
+
+    //in seconds = 5 min - 300s
+    breakSession: 5,
+};
+
+export default PomoTimer;
